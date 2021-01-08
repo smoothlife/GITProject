eebc924c124b456234baf01d7cdd9b3819168433
kyk9400 <kyk9400@nate.com>
2021-01-08 09:42:25 +0900

    Project init

---
+++ src/main/java/kh/eclass/statics/Configurator.java
@@ -0,0 +1,8 @@
+package kh.eclass.statics;
+
+public class Configurator {
+	public static int recordCountPerPage = 10;
+	public static int naviCountPerPage = 10;
+	
+	public static String boardFileRoot = "C:\\boardRepository\\";
+}
